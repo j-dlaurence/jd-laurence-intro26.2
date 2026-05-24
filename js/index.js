5d9be427de7596d4eb50bbd6ab5ba17305ba8e33
J-D Laurence <266778800+j-dlaurence@users.noreply.github.com>
2026-05-24 13:53:53 -0400

    lesson 7: js folder and index.js added, created footer and skills

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const body = document.body;
+const footer = document.createElement('footer');
+body.appendChild(footer);
+
+today = new Date();
+const thisYear = today.getFullYear();
+const copyright = document.createElement('p');
+copyright.innerHTML = `\u00A9 ${thisYear} J-D Laurence`;
+footer.appendChild(copyright);
+
+const skills = ['HTML', 'CSS', 'JavaScript', 'Python', 'Git', 'SQL', 'C++', 'Java'];
+const skillsSection = document.querySelector('#skills');
+const skillsList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++) {
+    const skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
