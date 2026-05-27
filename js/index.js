f0134974e5bb7e06bb8f5e0b70e369c42eb02c0d
J-D Laurence <266778800+j-dlaurence@users.noreply.github.com>
2026-05-27 19:25:20 -0400

    form and functionality added for lesson 8

--- js/index.js
+++ js/index.js
@@ -17,3 +17,28 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector(('[name="leave_message"]'));
+messageForm.addEventListener('submit', function(event){
+    event.preventDefault(); //this stops the page refreshing
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+    console.log(usersName, usersEmail, usersMessage);
+    
+    const messageSection = document.querySelector('#messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+                            <span>${usersMessage}</span>`;
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button'; //otherwise it will be a submit by default 
+    removeButton.addEventListener('click', function(event){
+        const entry = event.target.parentNode;
+        entry.remove();
+    })
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    event.target.reset(); //this clears the form
+})
